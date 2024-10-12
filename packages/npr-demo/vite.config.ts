9d776f76a2291c13dffb1dfe244fdb2f4a103f36
Simone Sturniolo <simonesturniolo@gmail.com>
2024-10-12 15:01:47 +0100

    Fixed problem that caused demo page to use the wrong name for the component in the code examples

--- packages/npr-demo/vite.config.ts
+++ packages/npr-demo/vite.config.ts
@@ -5,4 +5,10 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   base: './',
+  esbuild: {
+    minifyIdentifiers: false
+  },
+  build: {
+    minify: "esbuild"
+  }
 })
